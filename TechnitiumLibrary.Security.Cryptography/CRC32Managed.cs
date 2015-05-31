085064fcd444d80afa436fcecd845ed07c5e4913
Shreyas Zare <shreyas@technitium.com>
2015-05-31 15:52:22 +0530

    TechnitiumLibrary.Security.Cryptography: CRC32 and RC4 modules added

---
+++ TechnitiumLibrary.Security.Cryptography/CRC32Managed.cs
@@ -0,0 +1,92 @@
+﻿/*
+Technitium Library
+Copyright (C) 2015  Shreyas Zare (shreyas@technitium.com)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+using System;
+using System.Security.Cryptography;
+
+namespace TechnitiumLibrary.Security.Cryptography
+{
+    public class CRC32Managed : HashAlgorithm
+    {
+        #region variables
+
+        uint[] _crc32Table = new uint[256];
+
+        uint _crc32;
+        uint _polynomial;
+        uint _preCondition;
+
+        #endregion
+
+        #region contructor
+
+        public CRC32Managed(uint polynomial = 0xEDB88320U, uint preCondition = 0xFFFFFFFFU)
+        {
+            _polynomial = polynomial;
+            _preCondition = preCondition;
+            _crc32 = preCondition;
+
+            this.HashSizeValue = 32; //bits
+
+            uint value;
+
+            for (uint iByte = 0; iByte < 256; iByte++)
+            {
+                value = iByte;
+
+                for (int iBit = 0; iBit < 8; iBit++)
+                {
+                    if ((value & 1) == 1)
+                        value = (value >> 1) ^ _polynomial;
+                    else
+                        value >>= 1;
+                }
+
+                _crc32Table[iByte] = value;
+            }
+        }
+
+        #endregion
+
+        #region CRC32 Algorithm
+
+        protected override void HashCore(byte[] array, int ibStart, int cbSize)
+        {
+            for (int iByte = ibStart; iByte < (ibStart + cbSize); iByte++)
+            {
+                _crc32 = (_crc32 >> 8) ^ _crc32Table[(_crc32 & 0xFFU) ^ array[iByte]];
+            }
+        }
+
+        protected override byte[] HashFinal()
+        {
+            byte[] output = BitConverter.GetBytes(_crc32 ^ 0xFFFFFFFFU);
+            Array.Reverse(output);
+
+            return output;
+        }
+
+        public override void Initialize()
+        {
+            _crc32 = _preCondition;
+        }
+
+        #endregion
+    }
+}
---
+++ TechnitiumLibrary.Security.Cryptography/RC4Managed.cs
@@ -0,0 +1,187 @@
+﻿/*
+Technitium Library
+Copyright (C) 2015  Shreyas Zare (shreyas@technitium.com)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+using System;
+using System.Security.Cryptography;
+
+namespace TechnitiumLibrary.Security.Cryptography
+{
+    public class RC4Managed : SymmetricAlgorithm
+    {
+        #region constructor
+
+        public RC4Managed()
+        {
+            this.LegalKeySizesValue = new KeySizes[] { new KeySizes(128, 512, 128) };
+            this.KeySizeValue = 256;
+
+            GenerateKey();
+            GenerateIV();
+        }
+
+        public RC4Managed(int keySize)
+        {
+            this.LegalKeySizesValue = new KeySizes[] { new KeySizes(128, 256, 128) };
+            this.KeySize = keySize;
+
+            GenerateKey();
+            GenerateIV();
+        }
+
+        public RC4Managed(byte[] key, byte[] IV)
+        {
+            this.LegalKeySizesValue = new KeySizes[] { new KeySizes(128, 512, 128) };
+            this.KeySize = key.Length * 8;
+
+            this.KeyValue = key;
+            this.IVValue = IV;
+        }
+
+        #endregion
+
+        #region overrides
+
+        public override ICryptoTransform CreateDecryptor(byte[] rgbKey, byte[] rgbIV)
+        {
+            return new RC4Transform(rgbKey, rgbIV);
+        }
+
+        public override ICryptoTransform CreateEncryptor(byte[] rgbKey, byte[] rgbIV)
+        {
+            return new RC4Transform(rgbKey, rgbIV);
+        }
+
+        public override void GenerateIV()
+        {
+            RandomNumberGenerator rnd = new RNGCryptoServiceProvider();
+
+            byte[] iv = new byte[this.KeySizeValue / 8];
+            rnd.GetBytes(iv);
+
+            this.IVValue = iv;
+        }
+
+        public override void GenerateKey()
+        {
+            RandomNumberGenerator rnd = new RNGCryptoServiceProvider();
+
+            byte[] key = new byte[this.KeySizeValue / 8];
+            rnd.GetBytes(key);
+
+            this.KeyValue = key;
+        }
+
+        #endregion
+
+        class RC4Transform : ICryptoTransform
+        {
+            #region variables
+
+            byte[] s = new byte[256];
+            int xi = 0;
+            int xj = 0;
+
+            #endregion
+
+            #region constructor
+
+            public RC4Transform(byte[] key, byte[] IV)
+            {
+                //mix key with IV by taking HMAC
+                HMAC hmac = new HMACSHA256(key);
+                key = hmac.ComputeHash(IV);
+
+                //RC4 key scheduling algo
+                for (int i = 0; i < 256; i++)
+                    s[i] = (byte)i;
+
+                int j = 0;
+                byte tmp;
+
+                for (int i = 0; i < 256; i++)
+                {
+                    j = (j + s[i] + key[i % key.Length]) & 0xFF;
+
+                    tmp = s[i];
+                    s[i] = s[j];
+                    s[j] = tmp;
+                }
+            }
+
+            #endregion
+
+            #region Dispose
+
+            public void Dispose()
+            {
+                for (int i = 0; i < 256; i++)
+                    s[i] = 0;
+
+                xi = 0;
+                xj = 0;
+            }
+
+            #endregion
+
+            #region algorithm
+
+            public int TransformBlock(byte[] inputBuffer, int inputOffset, int inputCount, byte[] outputBuffer, int outputOffset)
+            {
+                byte temp;
+
+                for (int i = 0; i < inputCount; i++)
+                {
+                    xi = (xi + 1) & 0xFF;
+                    xj = (xj + s[xi]) & 0xFF;
+
+                    temp = s[xi];
+                    s[xi] = s[xj];
+                    s[xj] = temp;
+
+                    outputBuffer[outputOffset + i] = Convert.ToByte(inputBuffer[inputOffset + i] ^ s[(s[xi] + s[xj]) & 0xFF]);
+                }
+
+                return inputCount;
+            }
+
+            public byte[] TransformFinalBlock(byte[] inputBuffer, int inputOffset, int inputCount)
+            {
+                return new byte[] { };
+            }
+
+            #endregion
+
+            #region properties
+
+            public bool CanReuseTransform
+            { get { return false; } }
+
+            public bool CanTransformMultipleBlocks
+            { get { return true; } }
+
+            public int InputBlockSize
+            { get { return 8; } }
+
+            public int OutputBlockSize
+            { get { return 8; } }
+
+            #endregion
+        }
+    }
+}
