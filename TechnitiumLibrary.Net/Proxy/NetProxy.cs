201df65b0cfc9f7ef74379ea6beee7d2760a9f15
Shreyas Zare <shreyas@technitium.com>
2015-11-08 15:21:28 +0530

    Net: NetProxy class added to combine http & socks5 proxy together. WebProxyEx class extending WebProxy added to provide direct connect() method to get a tcp socket connection over http proxy.

---
+++ TechnitiumLibrary.Net/Proxy/NetProxy.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace TechnitiumLibrary.Net.Proxy
+{
+    public enum NetProxyType
+    {
+        None = 0,
+        Http = 1,
+        Socks5 = 2
+    }
+
+    public class NetProxy
+    {
+        #region variables
+
+        NetProxyType _type;
+
+        WebProxyEx _httpProxy;
+        SocksClient _socksProxy;
+
+        #endregion
+
+        #region constructor
+
+        public NetProxy(WebProxyEx httpProxy)
+        {
+            _type = NetProxyType.Http;
+            _httpProxy = httpProxy;
+        }
+
+        public NetProxy(SocksClient socksProxy)
+        {
+            _type = NetProxyType.Socks5;
+            _socksProxy = socksProxy;
+        }
+
+        #endregion
+
+        #region public
+
+        public bool IsProxyAvailable()
+        {
+            switch (_type)
+            {
+                case NetProxyType.Http:
+                    return WebUtilities.IsWebAccessible(null, this);
+
+                case NetProxyType.Socks5:
+                    return _socksProxy.IsProxyAvailable();
+
+                default:
+                    return false;
+            }
+        }
+
+        public void CheckProxyAccess()
+        {
+            switch (_type)
+            {
+                case NetProxyType.Http:
+                    WebUtilities.IsWebAccessible(null, this, true);
+                    break;
+
+                case NetProxyType.Socks5:
+                    _socksProxy.CheckProxyAccess();
+                    break;
+
+                default:
+                    throw new NotSupportedException("Proxy type not supported.");
+            }
+        }
+
+        #endregion
+
+        #region properties
+
+        public NetProxyType Type
+        { get { return _type; } }
+
+        public WebProxyEx HttpProxy
+        { get { return _httpProxy; } }
+
+        public SocksClient SocksProxy
+        { get { return _socksProxy; } }
+
+        #endregion
+    }
+}
---
+++ TechnitiumLibrary.Net/Proxy/WebProxyEx.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TechnitiumLibrary.Net.Proxy
+{
+    public class WebProxyEx : WebProxy
+    {
+        #region constructors
+
+        public WebProxyEx()
+            : base()
+        { }
+
+        public WebProxyEx(string Address)
+            : base(Address)
+        { }
+
+        public WebProxyEx(Uri Address)
+            : base(Address)
+        { }
+
+        public WebProxyEx(string Address, bool BypassOnLocal)
+            : base(Address, BypassOnLocal)
+        { }
+
+        public WebProxyEx(string Host, int Port)
+            : base(Host, Port)
+        { }
+
+        public WebProxyEx(Uri Address, bool BypassOnLocal)
+            : base(Address, BypassOnLocal)
+        { }
+
+        public WebProxyEx(string Address, bool BypassOnLocal, string[] BypassList)
+            : base(Address, BypassOnLocal, BypassList)
+        { }
+
+        public WebProxyEx(Uri Address, bool BypassOnLocal, string[] BypassList)
+            : base(Address, BypassOnLocal, BypassList)
+        { }
+
+        public WebProxyEx(string Address, bool BypassOnLocal, string[] BypassList, ICredentials Credentials)
+            : base(Address, BypassOnLocal, BypassList, Credentials)
+        { }
+
+        public WebProxyEx(Uri Address, bool BypassOnLocal, string[] BypassList, ICredentials Credentials)
+            : base(Address, BypassOnLocal, BypassList, Credentials)
+        { }
+
+        #endregion
+
+        #region public
+
+        public Socket Connect(IPEndPoint remoteEP)
+        {
+            return Connect(remoteEP.Address.ToString(), remoteEP.Port);
+        }
+
+        public Socket Connect(IPAddress address, int port)
+        {
+            return Connect(address.ToString(), port);
+        }
+
+        public Socket Connect(string address, int port)
+        {
+            Socket socket = null;
+
+            try
+            {
+                socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                socket.Connect(this.Address.Host, this.Address.Port);
+                socket.SendTimeout = 30000;
+                socket.ReceiveTimeout = 30000;
+
+                NetworkCredential credentials = null;
+
+                if (this.UseDefaultCredentials)
+                    credentials = CredentialCache.DefaultCredentials.GetCredential(this.Address, "BASIC");
+                else if (this.Credentials != null)
+                    credentials = this.Credentials.GetCredential(this.Address, "BASIC");
+
+                string httpConnectRequest = "CONNECT " + address + ":" + port + " HTTP/1.0\r\n";
+
+                if (credentials != null)
+                    httpConnectRequest += "Proxy-Authorization: Basic " + Convert.ToBase64String(Encoding.ASCII.GetBytes(credentials.UserName + ":" + credentials.Password)) + "\r\n";
+
+                httpConnectRequest += "\r\n";
+
+                socket.Send(Encoding.ASCII.GetBytes(httpConnectRequest));
+
+                byte[] buffer = new byte[128];
+                int bytesRecv = socket.Receive(buffer);
+
+                if (bytesRecv < 1)
+                    throw new WebException("No response was received from Http proxy server.");
+
+                string[] httpResponse = Encoding.ASCII.GetString(buffer, 0, bytesRecv).Split('\r')[0].Split(new char[] { ' ' }, 3);
+
+                switch (httpResponse[1])
+                {
+                    case "200":
+                        return socket;
+
+                    case "407":
+                        throw new WebException("The remote server returned an error: (" + httpResponse[1] + ") Proxy Authorization Required");
+
+                    default:
+                        throw new WebException("The remote server returned an error: (" + httpResponse[1] + ") " + httpResponse[2]);
+                }
+            }
+            catch
+            {
+                if (socket != null)
+                    socket.Dispose();
+
+                throw;
+            }
+        }
+
+        #endregion
+    }
+}
