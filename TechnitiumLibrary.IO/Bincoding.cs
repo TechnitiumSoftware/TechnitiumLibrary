97d8e18162817d2b9fe047219b81565fce01a812
Shreyas Zare <shreyas@technitium.com>
2016-01-17 15:30:44 +0530

    Bincoding: helper class for binary encoding of data added.

---
+++ TechnitiumLibrary.IO/Bincoding.cs
@@ -0,0 +1,696 @@
+﻿/*
+Technitium Library
+Copyright (C) 2016  Shreyas Zare (shreyas@technitium.com)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace TechnitiumLibrary.IO
+{
+    public enum BincodingType : byte
+    {
+        NULL = 0,
+        BOOLEAN = 1,
+        BYTE = 2,
+        SHORT = 3,
+        INTEGER = 4,
+        LONG = 5,
+        USHORT = 6,
+        UINTEGER = 7,
+        ULONG = 8,
+        BINARY = 9,
+        STRING = 10,
+        STREAM = 11,
+        LIST = 12,
+        KEY_VALUE_PAIR = 13,
+        DICTIONARY = 14
+    }
+
+    public class Bincoding
+    {
+        #region variables
+
+        BincodingType _type;
+        byte[] _value;
+        Stream _stream;
+        List<Bincoding> _list;
+        KeyValuePair<string, Bincoding> _keyValue;
+        Dictionary<string, Bincoding> _dictionary;
+
+        #endregion
+
+        #region constructor
+
+        public Bincoding(BincodingType type, byte[] value)
+        {
+            _type = type;
+            _value = value;
+        }
+
+        private Bincoding(Stream value)
+        {
+            _type = BincodingType.STREAM;
+            _stream = value;
+        }
+
+        private Bincoding(List<Bincoding> value)
+        {
+            _type = BincodingType.LIST;
+            _list = value;
+        }
+
+        private Bincoding(KeyValuePair<string, Bincoding> value)
+        {
+            _type = BincodingType.KEY_VALUE_PAIR;
+            _keyValue = value;
+        }
+
+        private Bincoding(Dictionary<string, Bincoding> value)
+        {
+            _type = BincodingType.DICTIONARY;
+            _dictionary = value;
+        }
+
+        #endregion
+
+        #region static
+
+        public static Bincoding GetNullValue()
+        {
+            return new Bincoding(BincodingType.NULL, null);
+        }
+
+        public static Bincoding GetValue(bool value)
+        {
+            return new Bincoding(BincodingType.BOOLEAN, BitConverter.GetBytes(value));
+        }
+
+        public static Bincoding GetValue(byte value)
+        {
+            return new Bincoding(BincodingType.BYTE, new byte[] { value });
+        }
+
+        public static Bincoding GetValue(short value)
+        {
+            return new Bincoding(BincodingType.SHORT, BitConverter.GetBytes(value));
+        }
+
+        public static Bincoding GetValue(int value)
+        {
+            return new Bincoding(BincodingType.INTEGER, BitConverter.GetBytes(value));
+        }
+
+        public static Bincoding GetValue(long value)
+        {
+            return new Bincoding(BincodingType.LONG, BitConverter.GetBytes(value));
+        }
+
+        public static Bincoding GetValue(ushort value)
+        {
+            return new Bincoding(BincodingType.USHORT, BitConverter.GetBytes(value));
+        }
+
+        public static Bincoding GetValue(uint value)
+        {
+            return new Bincoding(BincodingType.UINTEGER, BitConverter.GetBytes(value));
+        }
+
+        public static Bincoding GetValue(ulong value)
+        {
+            return new Bincoding(BincodingType.ULONG, BitConverter.GetBytes(value));
+        }
+
+        public static Bincoding GetValue(byte[] value)
+        {
+            return new Bincoding(BincodingType.BINARY, value);
+        }
+
+        public static Bincoding GetValue(IWriteStream value)
+        {
+            return new Bincoding(BincodingType.BINARY, value.ToArray());
+        }
+
+        public static Bincoding GetValue(string value)
+        {
+            return new Bincoding(BincodingType.STRING, Encoding.UTF8.GetBytes(value));
+        }
+
+        public static Bincoding GetValue(Stream value)
+        {
+            return new Bincoding(BincodingType.STREAM, null) { _stream = value };
+        }
+
+        public static Bincoding GetValue(List<Bincoding> value)
+        {
+            return new Bincoding(value);
+        }
+
+        public static Bincoding GetValue(KeyValuePair<string, Bincoding> value)
+        {
+            return new Bincoding(value);
+        }
+
+        public static Bincoding GetValue(string key, Bincoding value)
+        {
+            return new Bincoding(new KeyValuePair<string, Bincoding>(key, value));
+        }
+
+        public static Bincoding GetValue(Dictionary<string, Bincoding> value)
+        {
+            return new Bincoding(value);
+        }
+
+        #endregion
+
+        #region public
+
+        public Stream GetValueStream()
+        {
+            if (_type == BincodingType.STREAM)
+                return _stream;
+            else
+                return new MemoryStream(_value, false);
+        }
+
+        public bool GetBooleanValue()
+        {
+            return BitConverter.ToBoolean(_value, 0);
+        }
+
+        public byte GetByteValue()
+        {
+            return _value[0];
+        }
+
+        public short GetShortValue()
+        {
+            return BitConverter.ToInt16(_value, 0);
+        }
+
+        public int GetIntegerValue()
+        {
+            return BitConverter.ToInt32(_value, 0);
+        }
+
+        public long GetLongValue()
+        {
+            return BitConverter.ToInt64(_value, 0);
+        }
+
+        public ushort GetUShortValue()
+        {
+            return BitConverter.ToUInt16(_value, 0);
+        }
+
+        public uint GetUIntegerValue()
+        {
+            return BitConverter.ToUInt32(_value, 0);
+        }
+
+        public ulong GetULongValue()
+        {
+            return BitConverter.ToUInt64(_value, 0);
+        }
+
+        public string GetStringValue()
+        {
+            return Encoding.UTF8.GetString(_value);
+        }
+
+        public List<Bincoding> GetList()
+        {
+            return _list;
+        }
+
+        public KeyValuePair<string, Bincoding> GetKeyValuePair()
+        {
+            return _keyValue;
+        }
+
+        public Dictionary<string, Bincoding> GetDictionary()
+        {
+            return _dictionary;
+        }
+
+        #endregion
+
+        #region properties
+
+        public BincodingType Type
+        { get { return _type; } }
+
+        public byte[] Value
+        { get { return _value; } }
+
+        #endregion
+    }
+
+    public class BincodingEncoder
+    {
+        #region variables
+
+        Stream _s;
+
+        string _format;
+        byte _version;
+
+        #endregion
+
+        #region constructor
+
+        public BincodingEncoder(Stream s, string format = "BE", byte version = 0)
+        {
+            if (format.Length != 2)
+                throw new ArgumentException("Argument 'format' must be of 2 characters.");
+
+            s.Write(Encoding.ASCII.GetBytes(format), 0, 2);
+            s.WriteByte(version);
+
+            _s = s;
+            _format = format;
+            _version = version;
+        }
+
+        #endregion
+
+        #region public
+
+        public void Encode(Bincoding value)
+        {
+            _s.WriteByte((byte)value.Type);
+
+            switch (value.Type)
+            {
+                case BincodingType.NULL:
+                    break;
+
+                case BincodingType.BINARY:
+                case BincodingType.STRING:
+                    _s.Write(BitConverter.GetBytes(value.Value.Length), 0, 4);
+                    _s.Write(value.Value, 0, value.Value.Length);
+                    break;
+
+                case BincodingType.STREAM:
+                    Stream stream = value.GetValueStream();
+
+                    long count = stream.Length - stream.Position;
+                    _s.Write(BitConverter.GetBytes(count), 0, 8);
+
+                    OffsetStream.StreamCopy(stream, _s);
+                    break;
+
+                case BincodingType.LIST:
+                    List<Bincoding> list = value.GetList();
+
+                    _s.Write(BitConverter.GetBytes(list.Count), 0, 4);
+
+                    foreach (Bincoding item in list)
+                        Encode(item);
+
+                    break;
+
+                case BincodingType.KEY_VALUE_PAIR:
+                    KeyValuePair<string, Bincoding> keyValue = value.GetKeyValuePair();
+
+                    byte[] keyBuffer = Encoding.UTF8.GetBytes(keyValue.Key);
+                    _s.WriteByte(Convert.ToByte(keyBuffer.Length));
+                    _s.Write(keyBuffer, 0, keyBuffer.Length);
+
+                    Encode(keyValue.Value);
+
+                    break;
+
+                case BincodingType.DICTIONARY:
+                    Dictionary<string, Bincoding> dictionary = value.GetDictionary();
+
+                    _s.Write(BitConverter.GetBytes(dictionary.Count), 0, 4);
+
+                    foreach (KeyValuePair<string, Bincoding> item in dictionary)
+                        Encode(item);
+
+                    break;
+
+                default:
+                    _s.Write(value.Value, 0, value.Value.Length);
+                    break;
+            }
+        }
+
+        public void EncodeNull()
+        {
+            Encode(new Bincoding(BincodingType.NULL, null));
+        }
+
+        public void Encode(bool value)
+        {
+            Encode(Bincoding.GetValue(value));
+        }
+
+        public void Encode(byte value)
+        {
+            Encode(Bincoding.GetValue(value));
+        }
+
+        public void Encode(short value)
+        {
+            Encode(Bincoding.GetValue(value));
+        }
+
+        public void Encode(int value)
+        {
+            Encode(Bincoding.GetValue(value));
+        }
+
+        public void Encode(long value)
+        {
+            Encode(Bincoding.GetValue(value));
+        }
+
+        public void Encode(ushort value)
+        {
+            Encode(Bincoding.GetValue(value));
+        }
+
+        public void Encode(uint value)
+        {
+            Encode(Bincoding.GetValue(value));
+        }
+
+        public void Encode(ulong value)
+        {
+            Encode(Bincoding.GetValue(value));
+        }
+
+        public void Encode(byte[] value)
+        {
+            Encode(Bincoding.GetValue(value));
+        }
+
+        public void Encode(IWriteStream value)
+        {
+            Encode(Bincoding.GetValue(value));
+        }
+
+        public void Encode(string value)
+        {
+            Encode(Bincoding.GetValue(value));
+        }
+
+        public void Encode(Stream value)
+        {
+            Encode(Bincoding.GetValue(value));
+        }
+
+        public void Encode(List<Bincoding> value)
+        {
+            Encode(Bincoding.GetValue(value));
+        }
+
+        public void Encode(KeyValuePair<string, Bincoding> value)
+        {
+            Encode(Bincoding.GetValue(value));
+        }
+
+        public void Encode(string key, Bincoding value)
+        {
+            Encode(Bincoding.GetValue(key, value));
+        }
+
+        public void Encode(string key, bool value)
+        {
+            Encode(Bincoding.GetValue(key, Bincoding.GetValue(value)));
+        }
+
+        public void Encode(string key, byte value)
+        {
+            Encode(Bincoding.GetValue(key, Bincoding.GetValue(value)));
+        }
+
+        public void Encode(string key, short value)
+        {
+            Encode(Bincoding.GetValue(key, Bincoding.GetValue(value)));
+        }
+
+        public void Encode(string key, int value)
+        {
+            Encode(Bincoding.GetValue(key, Bincoding.GetValue(value)));
+        }
+
+        public void Encode(string key, long value)
+        {
+            Encode(Bincoding.GetValue(key, Bincoding.GetValue(value)));
+        }
+
+        public void Encode(string key, ushort value)
+        {
+            Encode(Bincoding.GetValue(key, Bincoding.GetValue(value)));
+        }
+
+        public void Encode(string key, uint value)
+        {
+            Encode(Bincoding.GetValue(key, Bincoding.GetValue(value)));
+        }
+
+        public void Encode(string key, ulong value)
+        {
+            Encode(Bincoding.GetValue(key, Bincoding.GetValue(value)));
+        }
+
+        public void Encode(string key, byte[] value)
+        {
+            Encode(Bincoding.GetValue(key, Bincoding.GetValue(value)));
+        }
+
+        public void Encode(string key, IWriteStream value)
+        {
+            Encode(Bincoding.GetValue(key, Bincoding.GetValue(value)));
+        }
+
+        public void Encode(string key, string value)
+        {
+            Encode(Bincoding.GetValue(key, Bincoding.GetValue(value)));
+        }
+
+        public void Encode(string key, List<Bincoding> value)
+        {
+            Encode(Bincoding.GetValue(key, Bincoding.GetValue(value)));
+        }
+
+        public void Encode(string key, Dictionary<string, Bincoding> value)
+        {
+            Encode(Bincoding.GetValue(key, Bincoding.GetValue(value)));
+        }
+
+        public void Encode(Dictionary<string, Bincoding> value)
+        {
+            Encode(Bincoding.GetValue(value));
+        }
+
+        #endregion
+
+        #region properties
+
+        public string Format
+        { get { return _format; } }
+
+        public byte Version
+        { get { return _version; } }
+
+        #endregion
+    }
+
+    public class BincodingDecoder
+    {
+        #region variables
+
+        Stream _s;
+
+        string _format;
+        byte _version;
+
+        Stream _lastStream;
+
+        #endregion
+
+        #region constructor
+
+        public BincodingDecoder(Stream s, string format = "BE")
+        {
+            if (format.Length != 2)
+                throw new ArgumentException("Argument 'format' must be of 2 characters.");
+
+            byte[] buffer = new byte[2];
+            OffsetStream.StreamRead(s, buffer, 0, 2);
+
+            if (format != Encoding.ASCII.GetString(buffer))
+                throw new InvalidDataException("Unable to decode: invalid data format.");
+
+            _s = s;
+            _format = format;
+            _version = (byte)s.ReadByte();
+        }
+
+        #endregion
+
+        #region public
+
+        public Bincoding DecodeNext()
+        {
+            if (_lastStream != null)
+            {
+                if ((_lastStream.Length - _lastStream.Position) > 0)
+                    OffsetStream.StreamCopy(_lastStream, new NullStream());
+
+                _lastStream = null;
+            }
+
+            BincodingType type = (BincodingType)_s.ReadByte();
+
+            switch (type)
+            {
+                case BincodingType.NULL:
+                    return new Bincoding(type, null);
+
+                case BincodingType.BOOLEAN:
+                case BincodingType.BYTE:
+                    {
+                        byte value = (byte)_s.ReadByte();
+                        return new Bincoding(type, new byte[] { value });
+                    }
+
+                case BincodingType.SHORT:
+                case BincodingType.USHORT:
+                    {
+                        byte[] value = new byte[2];
+                        OffsetStream.StreamRead(_s, value, 0, 2);
+
+                        return new Bincoding(type, value);
+                    }
+
+                case BincodingType.INTEGER:
+                case BincodingType.UINTEGER:
+                    {
+                        byte[] value = new byte[4];
+                        OffsetStream.StreamRead(_s, value, 0, 4);
+
+                        return new Bincoding(type, value);
+                    }
+
+                case BincodingType.LONG:
+                case BincodingType.ULONG:
+                    {
+                        byte[] value = new byte[8];
+                        OffsetStream.StreamRead(_s, value, 0, 8);
+
+                        return new Bincoding(type, value);
+                    }
+
+                case BincodingType.BINARY:
+                case BincodingType.STRING:
+                    {
+                        byte[] buffer = new byte[4];
+                        OffsetStream.StreamRead(_s, buffer, 0, 4);
+                        int count = BitConverter.ToInt32(buffer, 0);
+
+                        byte[] value = new byte[count];
+                        OffsetStream.StreamRead(_s, value, 0, count);
+
+                        return new Bincoding(type, value);
+                    }
+
+                case BincodingType.STREAM:
+                    {
+                        byte[] buffer = new byte[8];
+                        OffsetStream.StreamRead(_s, buffer, 0, 8);
+                        long count = BitConverter.ToInt64(buffer, 0);
+
+                        _lastStream = new OffsetStream(_s, _s.Position, count, true, false);
+
+                        return Bincoding.GetValue(_lastStream);
+                    }
+
+                case BincodingType.LIST:
+                    {
+                        byte[] buffer = new byte[4];
+                        OffsetStream.StreamRead(_s, buffer, 0, 4);
+                        int count = BitConverter.ToInt32(buffer, 0);
+
+                        List<Bincoding> list = new List<Bincoding>(count);
+
+                        for (int i = 0; i < count; i++)
+                            list.Add(DecodeNext());
+
+                        return Bincoding.GetValue(list);
+                    }
+
+                case BincodingType.KEY_VALUE_PAIR:
+                    {
+                        int keyLen = _s.ReadByte();
+                        byte[] keyBuffer = new byte[keyLen];
+                        OffsetStream.StreamRead(_s, keyBuffer, 0, keyLen);
+
+                        string key = Encoding.UTF8.GetString(keyBuffer, 0, keyLen);
+                        Bincoding value = DecodeNext();
+
+                        return Bincoding.GetValue(new KeyValuePair<string, Bincoding>(key, value));
+                    }
+
+                case BincodingType.DICTIONARY:
+                    {
+                        byte[] buffer = new byte[4];
+                        OffsetStream.StreamRead(_s, buffer, 0, 4);
+                        int count = BitConverter.ToInt32(buffer, 0);
+
+                        Dictionary<string, Bincoding> dictionary = new Dictionary<string, Bincoding>(count);
+                        int keyLen;
+                        byte[] keyBuffer = new byte[255];
+
+                        for (int i = 0; i < count; i++)
+                        {
+                            keyLen = _s.ReadByte();
+                            OffsetStream.StreamRead(_s, keyBuffer, 0, keyLen);
+
+                            string key = Encoding.UTF8.GetString(keyBuffer, 0, keyLen);
+                            Bincoding value = DecodeNext();
+
+                            dictionary.Add(key, value);
+                        }
+
+                        return Bincoding.GetValue(dictionary);
+                    }
+
+                default:
+                    throw new InvalidDataException("Invalid bincoding type encountered while decoding data.");
+            }
+        }
+
+        #endregion
+
+        #region properties
+
+        public string Format
+        { get { return _format; } }
+
+        public byte Version
+        { get { return _version; } }
+
+        #endregion
+    }
+}
