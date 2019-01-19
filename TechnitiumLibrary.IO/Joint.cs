dce370e8f36e74d9f04572aca645f30eabc230c0
Shreyas Zare <shreyas@technitium.com>
2019-01-19 13:23:28 +0530

    assembly version updated

--- TechnitiumLibrary.IO/Joint.cs
+++ TechnitiumLibrary.IO/Joint.cs
@@ -1,6 +1,6 @@
 ﻿/*
 Technitium Library
-Copyright (C) 2018  Shreyas Zare (shreyas@technitium.com)
+Copyright (C) 2019  Shreyas Zare (shreyas@technitium.com)
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
