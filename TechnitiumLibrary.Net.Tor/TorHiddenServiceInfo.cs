d521a4fcbbafc1a0e56de8c915fc1da17e804735
Shreyas Zare <shreyas@technitium.com>
2019-08-15 13:00:49 +0530

    fixed string comparision issues

--- TechnitiumLibrary.Net.Tor/TorHiddenServiceInfo.cs
+++ TechnitiumLibrary.Net.Tor/TorHiddenServiceInfo.cs
@@ -1,6 +1,6 @@
 ﻿/*
 Technitium Library
-Copyright (C) 2017  Shreyas Zare (shreyas@technitium.com)
+Copyright (C) 2019  Shreyas Zare (shreyas@technitium.com)
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -49,15 +49,15 @@ namespace TechnitiumLibrary.Net.Tor
                 if (response.StartsWith("250 "))
                     break;
 
-                if (response.StartsWith("250-ServiceID=", StringComparison.CurrentCultureIgnoreCase))
+                if (response.StartsWith("250-ServiceID=", StringComparison.OrdinalIgnoreCase))
                 {
                     _serviceId = response.Substring(14);
                 }
-                else if (response.StartsWith("250-PrivateKey=", StringComparison.CurrentCultureIgnoreCase))
+                else if (response.StartsWith("250-PrivateKey=", StringComparison.OrdinalIgnoreCase))
                 {
                     _privateKey = response.Substring(15);
                 }
-                else if (response.StartsWith("250-ClientAuth=", StringComparison.CurrentCultureIgnoreCase))
+                else if (response.StartsWith("250-ClientAuth=", StringComparison.OrdinalIgnoreCase))
                 {
                     string[] parts = response.Substring(15).Split(':');
                     if (parts.Length == 2)
