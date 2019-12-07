fb4b0d3ab5ebb02c5ca8d985f19a21a671b84e60
Shreyas Zare <shreyas@technitium.com>
2019-12-07 11:37:53 +0530

    ECDiffieHellman: code refactoring done.

--- TechnitiumLibrary.Security.Cryptography.EllipticCurve/ECDiffieHellman.cs
+++ TechnitiumLibrary.Security.Cryptography.EllipticCurve/ECDiffieHellman.cs
@@ -1,6 +1,6 @@
 ﻿/*
 Technitium Library
-Copyright (C) 2016  Shreyas Zare (shreyas@technitium.com)
+Copyright (C) 2019  Shreyas Zare (shreyas@technitium.com)
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -21,12 +21,11 @@ using System.Security.Cryptography;
 
 namespace TechnitiumLibrary.Security.Cryptography
 {
-    //wrapper class for .NET ECDiffieHellmanCng
     public class ECDiffieHellman : KeyAgreement
     {
         #region variables
 
-        object _ecdh;
+        readonly ECDiffieHellmanCng _ecdh;
 
         #endregion
 
@@ -35,28 +34,26 @@ namespace TechnitiumLibrary.Security.Cryptography
         public ECDiffieHellman(int keySize, KeyAgreementKeyDerivationFunction kdFunc, KeyAgreementKeyDerivationHashAlgorithm hashAlgo)
             : base(kdFunc, hashAlgo)
         {
-            ECDiffieHellmanCng ecdh = new ECDiffieHellmanCng(keySize);
-            ecdh.KeyDerivationFunction = ECDiffieHellmanKeyDerivationFunction.Hash;
+            _ecdh = new ECDiffieHellmanCng(keySize);
+            _ecdh.KeyDerivationFunction = ECDiffieHellmanKeyDerivationFunction.Hash;
 
             switch (hashAlgo)
             {
                 case KeyAgreementKeyDerivationHashAlgorithm.SHA256:
-                    ecdh.HashAlgorithm = CngAlgorithm.Sha256;
+                    _ecdh.HashAlgorithm = CngAlgorithm.Sha256;
                     break;
 
                 case KeyAgreementKeyDerivationHashAlgorithm.SHA384:
-                    ecdh.HashAlgorithm = CngAlgorithm.Sha384;
+                    _ecdh.HashAlgorithm = CngAlgorithm.Sha384;
                     break;
 
                 case KeyAgreementKeyDerivationHashAlgorithm.SHA512:
-                    ecdh.HashAlgorithm = CngAlgorithm.Sha512;
+                    _ecdh.HashAlgorithm = CngAlgorithm.Sha512;
                     break;
 
                 default:
                     throw new CryptoException("Key derivation hash algorithm not supported.");
             }
-
-            _ecdh = ecdh;
         }
 
         #endregion
@@ -65,12 +62,12 @@ namespace TechnitiumLibrary.Security.Cryptography
 
         public override byte[] GetPublicKey()
         {
-            return ((ECDiffieHellmanCng)_ecdh).PublicKey.ToByteArray();
+            return _ecdh.PublicKey.ToByteArray();
         }
 
         protected override byte[] ComputeKey(byte[] otherPartyPublicKey)
         {
-            return ((ECDiffieHellmanCng)_ecdh).DeriveKeyMaterial(ECDiffieHellmanCngPublicKey.FromByteArray(otherPartyPublicKey, CngKeyBlobFormat.EccPublicBlob));
+            return _ecdh.DeriveKeyMaterial(ECDiffieHellmanCngPublicKey.FromByteArray(otherPartyPublicKey, CngKeyBlobFormat.EccPublicBlob));
         }
 
         #endregion
