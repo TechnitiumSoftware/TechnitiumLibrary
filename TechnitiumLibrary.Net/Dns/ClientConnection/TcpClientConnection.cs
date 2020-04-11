72b25279ca5b24f5361fbb9a09265fb4bcc4c6e3
Shreyas Zare <shreyas@technitium.com>
2020-04-11 13:34:40 +0530

    TcpClientConnection: catched ObjectDisposedException to avoid exception logging when base connection is closed to make it similar to timeout. Retry attempt by dns client will initiate reconnection automatically.

--- TechnitiumLibrary.Net/Dns/ClientConnection/TcpClientConnection.cs
+++ TechnitiumLibrary.Net/Dns/ClientConnection/TcpClientConnection.cs
@@ -1,6 +1,6 @@
 ﻿/*
 Technitium Library
-Copyright (C) 2019  Shreyas Zare (shreyas@technitium.com)
+Copyright (C) 2020  Shreyas Zare (shreyas@technitium.com)
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -250,6 +250,11 @@ namespace TechnitiumLibrary.Net.Dns.ClientConnection
                     //timeout
                     return null;
                 }
+            }
+            catch (ObjectDisposedException)
+            {
+                //connection is closed, return null. retry attempt will reconnect back.
+                return null;
             }
             finally
             {
