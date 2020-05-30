e7cbc3d22c38024949d283910e2347461bea6366
Shreyas Zare <shreyas@technitium.com>
2020-05-30 18:28:58 +0530

    HttpsJsonClientConnection: implemented new timeout method and used Stopwatch.

--- TechnitiumLibrary.Net/Dns/ClientConnection/HttpsJsonClientConnection.cs
+++ TechnitiumLibrary.Net/Dns/ClientConnection/HttpsJsonClientConnection.cs
@@ -1,6 +1,6 @@
 ﻿/*
 Technitium Library
-Copyright (C) 2019  Shreyas Zare (shreyas@technitium.com)
+Copyright (C) 2020  Shreyas Zare (shreyas@technitium.com)
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -19,6 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 using Newtonsoft.Json;
 using System;
+using System.Diagnostics;
 using System.Text;
 using TechnitiumLibrary.Net.Proxy;
 
@@ -30,18 +31,16 @@ namespace TechnitiumLibrary.Net.Dns.ClientConnection
 
         public HttpsJsonClientConnection(NameServerAddress server, NetProxy proxy)
             : base(DnsTransportProtocol.HttpsJson, server, proxy)
-        {
-            _timeout = 5000;
-        }
+        { }
 
         #endregion
 
         #region public
 
-        public override DnsDatagram Query(DnsDatagram request)
+        public override DnsDatagram Query(DnsDatagram request, int timeout)
         {
             //DoH JSON format request 
-            DateTime sentAt = DateTime.UtcNow;
+            Stopwatch stopwatch = new Stopwatch();
             byte[] responseBuffer;
 
             using (WebClientEx wC = new WebClientEx())
@@ -50,7 +49,7 @@ namespace TechnitiumLibrary.Net.Dns.ClientConnection
                 wC.AddHeader("host", _server.DnsOverHttpEndPoint.Host + ":" + _server.DnsOverHttpEndPoint.Port);
                 wC.UserAgent = "DoH client";
                 wC.Proxy = _proxy;
-                wC.Timeout = _timeout;
+                wC.Timeout = timeout;
 
                 Uri queryUri;
 
@@ -59,16 +58,24 @@ namespace TechnitiumLibrary.Net.Dns.ClientConnection
                     if (_server.IPEndPoint == null)
                         _server.RecursiveResolveIPAddress();
 
+                    stopwatch.Start();
+
                     queryUri = new Uri(_server.DnsOverHttpEndPoint.Scheme + "://" + _server.IPEndPoint.ToString() + _server.DnsOverHttpEndPoint.PathAndQuery);
+
+                    stopwatch.Stop();
                 }
                 else
                 {
+                    stopwatch.Start();
+
                     queryUri = _server.DnsOverHttpEndPoint;
+
+                    stopwatch.Stop();
                 }
 
                 wC.QueryString.Clear();
                 wC.QueryString.Add("name", request.Question[0].Name);
-                wC.QueryString.Add("type", Convert.ToString(((int)request.Question[0].Type)));
+                wC.QueryString.Add("type", Convert.ToString((int)request.Question[0].Type));
 
                 responseBuffer = wC.DownloadData(queryUri);
             }
@@ -77,7 +84,7 @@ namespace TechnitiumLibrary.Net.Dns.ClientConnection
             dynamic jsonResponse = JsonConvert.DeserializeObject(Encoding.ASCII.GetString(responseBuffer));
 
             DnsDatagram response = new DnsDatagram(jsonResponse);
-            response.SetMetadata(new DnsDatagramMetadata(_server, _protocol, responseBuffer.Length, (DateTime.UtcNow - sentAt).TotalMilliseconds));
+            response.SetMetadata(new DnsDatagramMetadata(_server, _protocol, responseBuffer.Length, stopwatch.Elapsed.TotalMilliseconds));
 
             return response;
         }
