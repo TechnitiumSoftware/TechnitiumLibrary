4d8b68785f672837a3de4f28d442106ac8a7dc5a
Shreyas Zare <shreyas@technitium.com>
2020-08-22 16:52:43 +0530

    HttpsClientConnection: updated implementation to use HttpClient with async methods.

--- TechnitiumLibrary.Net/Dns/ClientConnection/HttpsClientConnection.cs
+++ TechnitiumLibrary.Net/Dns/ClientConnection/HttpsClientConnection.cs
@@ -20,6 +20,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 using System;
 using System.Diagnostics;
 using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
 using System.Threading.Tasks;
 using TechnitiumLibrary.Net.Proxy;
 
@@ -27,46 +30,82 @@ namespace TechnitiumLibrary.Net.Dns.ClientConnection
 {
     public class HttpsClientConnection : DnsClientConnection
     {
+        #region variables
+
+        readonly HttpClient _httpClient;
+
+        bool _pooled;
+        DateTime _lastQueried;
+
+        #endregion
+
         #region constructor
 
         public HttpsClientConnection(NameServerAddress server, NetProxy proxy)
             : base(DnsTransportProtocol.Https, server, proxy)
-        { }
+        {
+            if (proxy == null)
+            {
+                _httpClient = new HttpClient();
+            }
+            else
+            {
+                HttpClientHandler handler = new HttpClientHandler();
+                handler.Proxy = proxy;
+
+                _httpClient = new HttpClient(handler);
+            }
+
+            _httpClient.DefaultRequestHeaders.Add("accept", "application/dns-message");
+            _httpClient.DefaultRequestHeaders.Add("host", _server.DnsOverHttpEndPoint.Host + ":" + _server.DnsOverHttpEndPoint.Port);
+            _httpClient.DefaultRequestHeaders.Add("user-agent", "DoH client");
+        }
+
+        #endregion
+
+        #region IDisposable
+
+        bool _disposed;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            if (disposing && !_pooled)
+            {
+                if (_httpClient != null)
+                    _httpClient.Dispose();
+            }
+
+            _disposed = true;
+        }
 
         #endregion
 
         #region public
 
-        public override async Task<DnsDatagram> QueryAsync(DnsDatagram request, int timeout)
+        public override async Task<DnsDatagram> QueryAsync(DnsDatagram request, int timeout, int retries, CancellationToken cancellationToken)
         {
-            //serialize request
-            byte[] requestBuffer;
+            _lastQueried = DateTime.UtcNow;
 
-            using (MemoryStream mS = new MemoryStream(32))
+            HttpRequestMessage httpRequest;
             {
-                request.WriteToUdp(mS);
-                requestBuffer = mS.ToArray();
-            }
+                //serialize request
+                byte[] requestBuffer;
 
-            //DoH wire format request
-            Stopwatch stopwatch = new Stopwatch();
-            byte[] responseBuffer;
-
-            using (WebClientEx wC = new WebClientEx())
-            {
-                wC.AddHeader("content-type", "application/dns-message");
-                wC.AddHeader("accept", "application/dns-message");
-                wC.AddHeader("host", _server.DnsOverHttpEndPoint.Host + ":" + _server.DnsOverHttpEndPoint.Port);
-                wC.UserAgent = "DoH client";
-                wC.Proxy = _proxy;
-                wC.Timeout = timeout;
+                using (MemoryStream mS = new MemoryStream(32))
+                {
+                    request.WriteToUdp(mS);
+                    requestBuffer = mS.ToArray();
+                }
 
                 Uri queryUri;
 
                 if (_proxy == null)
                 {
-                    if (_server.IPEndPoint == null)
-                        _server.RecursiveResolveIPAddress();
+                    if (_server.IsIPEndPointStale)
+                        await _server.RecursiveResolveIPAddressAsync();
 
                     queryUri = new Uri(_server.DnsOverHttpEndPoint.Scheme + "://" + _server.IPEndPoint.ToString() + _server.DnsOverHttpEndPoint.PathAndQuery);
                 }
@@ -78,25 +117,66 @@ namespace TechnitiumLibrary.Net.Dns.ClientConnection
                         queryUri = new Uri(_server.DnsOverHttpEndPoint.Scheme + "://" + _server.IPEndPoint.ToString() + _server.DnsOverHttpEndPoint.PathAndQuery);
                 }
 
-                stopwatch.Start();
-
-                responseBuffer = await wC.UploadDataTaskAsync(queryUri, requestBuffer);
-
-                stopwatch.Stop();
+                httpRequest = new HttpRequestMessage(HttpMethod.Post, queryUri);
+                httpRequest.Content = new ByteArrayContent(requestBuffer);
+                httpRequest.Content.Headers.ContentType = new MediaTypeHeaderValue("application/dns-message");
             }
 
-            //parse response
-            using (MemoryStream mS = new MemoryStream(responseBuffer, false))
+            //DoH wire format request
+            Stopwatch stopwatch = new Stopwatch();
+            int retry = 0;
+            while (retry < retries) //retry loop
             {
-                DnsDatagram response = new DnsDatagram(mS, false);
+                retry++;
 
-                response.SetMetadata(new DnsDatagramMetadata(_server, _protocol, responseBuffer.Length, stopwatch.Elapsed.TotalMilliseconds));
+                if (cancellationToken.IsCancellationRequested)
+                    return await Task.FromCanceled<DnsDatagram>(cancellationToken); //task cancelled
 
-                if (response.Identifier == request.Identifier)
-                    return response;
+                stopwatch.Start();
+
+                Task<HttpResponseMessage> task = _httpClient.SendAsync(httpRequest, cancellationToken);
+
+                using (CancellationTokenSource timeoutCancellationTokenSource = new CancellationTokenSource())
+                {
+                    using (CancellationTokenRegistration ctr = cancellationToken.Register(delegate () { timeoutCancellationTokenSource.Cancel(); }))
+                    {
+                        if (await Task.WhenAny(task, Task.Delay(timeout, timeoutCancellationTokenSource.Token)) != task)
+                            continue; //request timed out; retry
+                    }
+
+                    timeoutCancellationTokenSource.Cancel(); //to stop delay task
+                }
+
+                byte[] responseBuffer = await (await task).Content.ReadAsByteArrayAsync();
+
+                stopwatch.Stop();
+
+                //parse response
+                using (MemoryStream mS = new MemoryStream(responseBuffer, false))
+                {
+                    DnsDatagram response = DnsDatagram.ReadFromUdp(mS);
+
+                    response.SetMetadata(new DnsDatagramMetadata(_server, _protocol, responseBuffer.Length, stopwatch.Elapsed.TotalMilliseconds));
+
+                    if (response.Identifier == request.Identifier)
+                        return response;
+                }
             }
 
             return null;
+        }
+
+        #endregion
+
+        #region properties
+
+        public DateTime LastQueried
+        { get { return _lastQueried; } }
+
+        internal bool Pooled
+        {
+            get { return _pooled; }
+            set { _pooled = value; }
         }
 
         #endregion
