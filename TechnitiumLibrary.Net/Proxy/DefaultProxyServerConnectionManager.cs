c9ccb6927b7314ff2585f24b1967cc2cc7b02a50
Shreyas Zare <shreyas@technitium.com>
2020-08-22 17:51:46 +0530

    DefaultProxyServerConnectionManager: added a default class to handle proxy server connections.

---
+++ TechnitiumLibrary.Net/Proxy/DefaultProxyServerConnectionManager.cs
@@ -0,0 +1,40 @@
+﻿/*
+Technitium Library
+Copyright (C) 2020  Shreyas Zare (shreyas@technitium.com)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace TechnitiumLibrary.Net.Proxy
+{
+    class DefaultProxyServerConnectionManager : IProxyServerConnectionManager
+    {
+        public async Task<Socket> ConnectAsync(EndPoint remoteEP)
+        {
+            if (remoteEP.AddressFamily == AddressFamily.Unspecified)
+                remoteEP = await remoteEP.GetIPEndPointAsync();
+
+            Socket socket = new Socket(remoteEP.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            await socket.ConnectAsync(remoteEP);
+
+            return socket;
+        }
+    }
+}
