6015d878263a2aa994714696065ca78a9724c7db
Shreyas Zare <shreyas@technitium.com>
2021-06-05 12:26:08 +0530

    HttpContentStream: fixed stream buffer length bug in Read() and ReadAsync().

--- TechnitiumLibrary.Net/Http/HttpContentStream.cs
+++ TechnitiumLibrary.Net/Http/HttpContentStream.cs
@@ -1,6 +1,6 @@
 ﻿/*
 Technitium Library
-Copyright (C) 2020  Shreyas Zare (shreyas@technitium.com)
+Copyright (C) 2021  Shreyas Zare (shreyas@technitium.com)
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -146,7 +146,7 @@ namespace TechnitiumLibrary.Net.Http
                     return bytesRead;
 
                 _offset = 0;
-                _length = bytesRemaining;
+                _length = bytesRead;
             }
         }
 
@@ -184,7 +184,7 @@ namespace TechnitiumLibrary.Net.Http
                     return bytesRead;
 
                 _offset = 0;
-                _length = bytesRemaining;
+                _length = bytesRead;
             }
         }
 
