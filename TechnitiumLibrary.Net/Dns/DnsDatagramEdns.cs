f33c7c451724aa477965b108b64466db3ba9d68a
Shreyas Zare <shreyas@technitium.com>
2021-10-30 12:02:10 +0530

    added DnsDatagramEdns

---
+++ TechnitiumLibrary.Net/Dns/DnsDatagramEdns.cs
@@ -0,0 +1,105 @@
+﻿/*
+Technitium Library
+Copyright (C) 2021  Shreyas Zare (shreyas@technitium.com)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using TechnitiumLibrary.Net.Dns.ResourceRecords;
+
+namespace TechnitiumLibrary.Net.Dns
+{
+    [Flags]
+    public enum EDnsHeaderFlags : ushort
+    {
+        None = 0,
+        DNSSEC_OK = 0x8000
+    }
+
+    public class DnsDatagramEdns
+    {
+        #region variables
+
+        readonly ushort _udpPayloadSize;
+        readonly DnsResponseCode _extendedRCODE;
+        readonly byte _version;
+        readonly EDnsHeaderFlags _flags;
+
+        #endregion
+
+        #region constructor
+
+        public DnsDatagramEdns(ushort udpPayloadSize, DnsResponseCode extendedRCODE, byte version, EDnsHeaderFlags flags)
+        {
+            _udpPayloadSize = udpPayloadSize;
+            _extendedRCODE = extendedRCODE;
+            _version = version;
+            _flags = flags;
+        }
+
+        #endregion
+
+        #region static
+
+        public static DnsDatagramEdns ReadOPTFrom(IReadOnlyList<DnsResourceRecord> additional, DnsResponseCode RCODE)
+        {
+            DnsResourceRecord opt = null;
+
+            foreach (DnsResourceRecord record in additional)
+            {
+                if (record.Type == DnsResourceRecordType.OPT)
+                {
+                    opt = record;
+                    break;
+                }
+            }
+
+            if (opt is null)
+                return null;
+
+            return new DnsDatagramEdns((ushort)opt.Class, (DnsResponseCode)(((opt.OriginalTtlValue & 0xff000000u) >> 20) | ((uint)RCODE & 0xfu)), (byte)((opt.OriginalTtlValue >> 16) & 0xffu), (EDnsHeaderFlags)(opt.OriginalTtlValue & 0xffffu));
+        }
+
+        public static DnsResourceRecord GetOPTFor(ushort udpPayloadSize = DnsDatagram.EDNS_DEFAULT_UDP_PAYLOAD_SIZE, DnsResponseCode extendedRCODE = DnsResponseCode.NoError, byte version = 0, EDnsHeaderFlags flags = EDnsHeaderFlags.None)
+        {
+            return new DnsResourceRecord("", DnsResourceRecordType.OPT, (DnsClass)udpPayloadSize, ((((uint)extendedRCODE) & 0x00000ff0u) << 20) | (((uint)version) << 16) | ((uint)flags), DnsOPTRecord.Empty);
+        }
+
+        #endregion
+
+        #region properties
+
+        public ushort UdpPayloadSize
+        { get { return _udpPayloadSize; } }
+
+        public DnsResponseCode ExtendedRCODE
+        { get { return _extendedRCODE; } }
+
+        public byte Version
+        { get { return _version; } }
+
+        [IgnoreDataMember]
+        public EDnsHeaderFlags Flags
+        { get { return _flags; } }
+
+        public bool DNSSEC_OK
+        { get { return _flags.HasFlag(EDnsHeaderFlags.DNSSEC_OK); } }
+
+        #endregion
+    }
+}
