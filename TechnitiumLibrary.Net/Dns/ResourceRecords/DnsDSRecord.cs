950eb8e7b3f6886e5ab221023cfbe9b3cf8b2208
Shreyas Zare <shreyas@technitium.com>
2021-12-19 15:56:14 +0530

    DnsDSRecord: completed full implementation.

--- TechnitiumLibrary.Net/Dns/ResourceRecords/DnsDSRecord.cs
+++ TechnitiumLibrary.Net/Dns/ResourceRecords/DnsDSRecord.cs
@@ -55,6 +55,8 @@ namespace TechnitiumLibrary.Net.Dns.ResourceRecords
             _algorithm = algorithm;
             _digestType = digestType;
             _digest = digest;
+
+            Serialize();
         }
 
         public DnsDSRecord(Stream s)
@@ -63,7 +65,51 @@ namespace TechnitiumLibrary.Net.Dns.ResourceRecords
 
         public DnsDSRecord(dynamic jsonResourceRecord)
         {
-            throw new NotSupportedException();
+            _rdLength = Convert.ToUInt16(jsonResourceRecord.data.Value.Length);
+
+            string[] parts = (jsonResourceRecord.data.Value as string).Split(' ');
+
+            _keyTag = ushort.Parse(parts[0]);
+            _algorithm = Enum.Parse<DnssecAlgorithm>(parts[1].Replace("-", "_"), true);
+            _digestType = Enum.Parse<DnssecDigestType>(parts[2], true);
+            _digest = Convert.FromHexString(parts[3]);
+
+            Serialize();
+        }
+
+        #endregion
+
+        #region static
+
+        public static bool IsAlgorithmSupported(IReadOnlyList<DnsResourceRecord> dsRecords)
+        {
+            foreach (DnsResourceRecord record in dsRecords)
+            {
+                if (record.Type != DnsResourceRecordType.DS)
+                    throw new InvalidOperationException();
+
+                if ((record.RDATA as DnsDSRecord).IsAlgorithmSupported())
+                    return true;
+            }
+
+            return false;
+        }
+
+        #endregion
+
+        #region private
+
+        private void Serialize()
+        {
+            using (MemoryStream mS = new MemoryStream(2 + 1 + 1 + _digest.Length))
+            {
+                DnsDatagram.WriteUInt16NetworkOrder(_keyTag, mS);
+                mS.WriteByte((byte)_algorithm);
+                mS.WriteByte((byte)_digestType);
+                mS.Write(_digest);
+
+                _rData = mS.ToArray();
+            }
         }
 
         #endregion
@@ -85,25 +131,45 @@ namespace TechnitiumLibrary.Net.Dns.ResourceRecords
 
         protected override void WriteRecordData(Stream s, List<DnsDomainOffset> domainEntries, bool canonicalForm)
         {
-            if (_rData is null)
-            {
-                using (MemoryStream mS = new MemoryStream(2 + 1 + 1 + _digest.Length))
-                {
-                    DnsDatagram.WriteUInt16NetworkOrder(_keyTag, mS);
-                    mS.WriteByte((byte)_algorithm);
-                    mS.WriteByte((byte)_digestType);
-                    mS.Write(_digest);
-
-                    _rData = mS.ToArray();
-                }
-            }
-
             s.Write(_rData);
         }
 
         #endregion
 
         #region public
+
+        public bool IsAlgorithmSupported()
+        {
+            switch (_algorithm)
+            {
+                case DnssecAlgorithm.RSAMD5:
+                case DnssecAlgorithm.RSASHA1:
+                case DnssecAlgorithm.RSASHA256:
+                case DnssecAlgorithm.RSASHA512:
+                case DnssecAlgorithm.RSASHA1_NSEC3_SHA1:
+                case DnssecAlgorithm.ECDSAP256SHA256:
+                case DnssecAlgorithm.ECDSAP384SHA384:
+                    //supported algorithm
+                    break;
+
+                default:
+                    return false;
+            }
+
+            switch (_digestType)
+            {
+                case DnssecDigestType.SHA1:
+                case DnssecDigestType.SHA256:
+                case DnssecDigestType.SHA384:
+                    //supported digest algorithm
+                    break;
+
+                default:
+                    return false;
+            }
+
+            return true;
+        }
 
         public override bool Equals(object obj)
         {
@@ -140,7 +206,7 @@ namespace TechnitiumLibrary.Net.Dns.ResourceRecords
 
         public override string ToString()
         {
-            return _keyTag + " " + (byte)_algorithm + " " + (byte)_digestType + " ( " + Convert.ToHexString(_digest) + " )";
+            return _keyTag + " " + (byte)_algorithm + " " + (byte)_digestType + " " + Convert.ToHexString(_digest);
         }
 
         #endregion
