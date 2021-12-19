4c9a88e8a1a4044bd9dae0a1ccfb2df6015673cd
Shreyas Zare <shreyas@technitium.com>
2021-12-19 15:57:39 +0530

    DnsRRSIGRecord: completed implementation.

--- TechnitiumLibrary.Net/Dns/ResourceRecords/DnsRRSIGRecord.cs
+++ TechnitiumLibrary.Net/Dns/ResourceRecords/DnsRRSIGRecord.cs
@@ -26,6 +26,14 @@ using TechnitiumLibrary.IO;
 
 namespace TechnitiumLibrary.Net.Dns.ResourceRecords
 {
+    public enum DnssecSignatureStatus
+    {
+        Unknown = 0,
+        Valid = 1,
+        Bogus = 2,
+        NoDnsKey = 3
+    }
+
     public class DnsRRSIGRecord : DnsResourceRecordData
     {
         #region variables
@@ -65,7 +73,79 @@ namespace TechnitiumLibrary.Net.Dns.ResourceRecords
 
         public DnsRRSIGRecord(dynamic jsonResourceRecord)
         {
-            throw new NotSupportedException();
+            _rdLength = Convert.ToUInt16(jsonResourceRecord.data.Value.Length);
+
+            string[] parts = (jsonResourceRecord.data.Value as string).Split(' ');
+
+            _typeCovered = Enum.Parse<DnsResourceRecordType>(parts[0], true);
+            _algorithm = Enum.Parse<DnssecAlgorithm>(parts[1].Replace("-", "_"), true);
+            _labels = byte.Parse(parts[2]);
+            _originalTtl = uint.Parse(parts[3]);
+            _signatureExpiration = uint.Parse(parts[4]);
+            _signatureInception = uint.Parse(parts[5]);
+            _keyTag = ushort.Parse(parts[6]);
+            _signersName = parts[7].TrimEnd('.');
+            _signature = Convert.FromBase64String(parts[8]);
+        }
+
+        #endregion
+
+        #region static
+
+        public static bool IsWildcard(DnsResourceRecord rrsigRecord)
+        {
+            if (rrsigRecord.RDATA is DnsRRSIGRecord rrsig)
+                return GetLabelCount(rrsigRecord.Name) > rrsig._labels;
+
+            throw new InvalidOperationException();
+        }
+
+        #endregion
+
+        #region private
+
+        private static string GetWildcardDomain(string domain, int labelCount)
+        {
+            string[] labels = domain.Split('.');
+            string wildcard = null;
+
+            for (int i = 0; i < labelCount; i++)
+            {
+                if (wildcard is null)
+                    wildcard = labels[labels.Length - 1 - i];
+                else
+                    wildcard = labels[labels.Length - 1 - i] + "." + wildcard;
+            }
+
+            return "*." + wildcard;
+        }
+
+        private static int GetLabelCount(string domain)
+        {
+            if (domain.Length == 0)
+                return 0;
+
+            int count = 0;
+
+            foreach (string label in domain.Split('.'))
+            {
+                if (label == "*")
+                    continue;
+
+                count++;
+            }
+
+            return count;
+        }
+
+        private void Serialize()
+        {
+            using (MemoryStream mS = new MemoryStream(2 + 1 + 1 + 4 + 4 + 4 + 2 + DnsDatagram.GetSerializeDomainNameLength(_signersName) + _signature.Length))
+            {
+                WriteTo(mS, false);
+
+                _rData = mS.ToArray();
+            }
         }
 
         #endregion
@@ -88,14 +168,7 @@ namespace TechnitiumLibrary.Net.Dns.ResourceRecords
         protected override void WriteRecordData(Stream s, List<DnsDomainOffset> domainEntries, bool canonicalForm)
         {
             if (_rData is null)
-            {
-                using (MemoryStream mS = new MemoryStream(2 + 1 + 1 + 4 + 4 + 4 + 2 + DnsDatagram.GetSerializeDomainNameLength(_signersName) + _signature.Length))
-                {
-                    WriteTo(mS, canonicalForm);
-
-                    _rData = mS.ToArray();
-                }
-            }
+                Serialize();
 
             s.Write(_rData);
         }
@@ -123,86 +196,183 @@ namespace TechnitiumLibrary.Net.Dns.ResourceRecords
 
         #region public
 
-        public bool IsSignatureValid(IReadOnlyList<DnsResourceRecord> answer, IReadOnlyList<DnsResourceRecord> dnsKeys)
+        public DnssecSignatureStatus IsSignatureValid(IReadOnlyList<DnsResourceRecord> records, IReadOnlyList<DnsResourceRecord> dnsKeyRecords)
         {
+            //The validator's notion of the current time MUST be less than or equal to the time listed in the RRSIG RR's Expiration field.
+            if (SignatureExpiration < DateTime.UtcNow)
+                return DnssecSignatureStatus.Bogus;
+
+            //The validator's notion of the current time MUST be greater than or equal to the time listed in the RRSIG RR's Inception field.
+            if (SignatureInception > DateTime.UtcNow)
+                return DnssecSignatureStatus.Bogus;
+
             using (MemoryStream mS = new MemoryStream(512))
             {
                 //RRSIG_RDATA
                 WriteTo(mS, true);
 
                 //RR(i) = owner | type | class | TTL | RDATA length | RDATA
-                List<byte[]> rrBufferList = new List<byte[]>(answer.Count);
+
+                string name;
+                {
+                    //The number of labels in the RRset owner name MUST be greater than or equal to the value in the RRSIG RR's Labels field.
+                    int labelCount = GetLabelCount(records[0].Name);
+
+                    if (_labels == labelCount)
+                    {
+                        //name = fqdn
+                        name = records[0].Name.ToLower();
+                    }
+                    else if (_labels < labelCount)
+                    {
+                        //name = "*." | the rightmost rrsig_label labels of the fqdn
+                        name = GetWildcardDomain(records[0].Name, _labels).ToLower();
+
+                        if (_typeCovered == DnsResourceRecordType.NSEC)
+                        {
+                            //wildcard NSEC can be abused by an attacker to serve NXDomain or NoData responses
+                            //fix NSEC record owner name to the original wildcard owner name
+                            records[0].FixNameForNSEC(name);
+                        }
+                    }
+                    else
+                    {
+                        //the RRSIG RR did not pass the necessary validation checks and MUST NOT be used to authenticate this RRset.
+                        return DnssecSignatureStatus.Bogus;
+                    }
+                }
+
+                List<SerializedResourceRecord> rrList = new List<SerializedResourceRecord>(records.Count);
 
                 //select and serialize records
                 using (MemoryStream rrBuffer = new MemoryStream(512))
                 {
-                    foreach (DnsResourceRecord record in answer)
+                    //serialize RDATA
+                    foreach (DnsResourceRecord record in records)
                     {
-                        if (record.Type == _typeCovered)
-                        {
-                            record.WriteTo(rrBuffer, null, true, _originalTtl);
-                            rrBufferList.Add(rrBuffer.ToArray());
+                        byte[] firstPart;
+                        byte[] rdataPart;
 
-                            rrBuffer.Position = 0;
-                        }
+                        //serialize RDATA
+                        record.RDATA.WriteCanonicalRecordData(rrBuffer);
+
+                        rdataPart = rrBuffer.ToArray();
+                        rrBuffer.SetLength(0);
+
+                        //serialize owner | type | class | TTL | RDATA length
+                        DnsDatagram.SerializeDomainName(name, rrBuffer);
+                        DnsDatagram.WriteUInt16NetworkOrder((ushort)records[0].Type, rrBuffer);
+                        DnsDatagram.WriteUInt16NetworkOrder((ushort)records[0].Class, rrBuffer);
+                        DnsDatagram.WriteUInt32NetworkOrder(_originalTtl, rrBuffer);
+                        DnsDatagram.WriteUInt16NetworkOrder(Convert.ToUInt16(rdataPart.Length), rrBuffer);
+
+                        firstPart = rrBuffer.ToArray();
+                        rrBuffer.SetLength(0);
+
+                        //add to list
+                        rrList.Add(new SerializedResourceRecord(firstPart, rdataPart));
                     }
                 }
 
-                //Canonical RR Ordering
-                rrBufferList.Sort(DnsNSECRecord.CanonicalComparison);
+                //Canonical RR Ordering by sorting RDATA portion of the canonical form of each RR
+                rrList.Sort();
 
-                //write into main buffer
-                foreach (byte[] buffer in rrBufferList)
-                    mS.Write(buffer);
+                //write sorted RR into main buffer
+                foreach (SerializedResourceRecord rr in rrList)
+                {
+                    rr.WriteTo(mS);
+                }
 
                 //verify
                 HashAlgorithmName hashAlgorithm;
 
                 switch (_algorithm)
                 {
-                    case DnssecAlgorithm.RSA_MD5:
+                    case DnssecAlgorithm.RSAMD5:
                         hashAlgorithm = HashAlgorithmName.MD5;
                         break;
 
-                    case DnssecAlgorithm.DSA_SHA1:
-                    case DnssecAlgorithm.RSA_SHA1:
+                    case DnssecAlgorithm.DSA:
+                    case DnssecAlgorithm.RSASHA1:
+                    case DnssecAlgorithm.DSA_NSEC3_SHA1:
+                    case DnssecAlgorithm.RSASHA1_NSEC3_SHA1:
                         hashAlgorithm = HashAlgorithmName.SHA1;
                         break;
 
-                    case DnssecAlgorithm.RSA_SHA256:
-                    case DnssecAlgorithm.ECDSA_P256_SHA256:
+                    case DnssecAlgorithm.RSASHA256:
+                    case DnssecAlgorithm.ECDSAP256SHA256:
                         hashAlgorithm = HashAlgorithmName.SHA256;
                         break;
 
-                    case DnssecAlgorithm.ECDSA_P384_SHA384:
+                    case DnssecAlgorithm.ECDSAP384SHA384:
                         hashAlgorithm = HashAlgorithmName.SHA384;
                         break;
 
-                    case DnssecAlgorithm.RSA_SHA512:
+                    case DnssecAlgorithm.RSASHA512:
                         hashAlgorithm = HashAlgorithmName.SHA512;
                         break;
 
                     default:
-                        throw new NotSupportedException("Hash algorithm is not supported: " + _algorithm.ToString());
+                        throw new NotSupportedException("DNSSEC hash algorithm is not supported: " + _algorithm.ToString());
                 }
 
-                foreach (DnsResourceRecord record in dnsKeys)
+                bool isBogus = false;
+
+                foreach (DnsResourceRecord dnsKeyRecord in dnsKeyRecords)
                 {
-                    if (record.Type == DnsResourceRecordType.DNSKEY)
+                    if (dnsKeyRecord.Type != DnsResourceRecordType.DNSKEY)
+                        continue;
+
+                    //The RRSIG RR's Signer's Name, Algorithm, and Key Tag fields MUST match the owner name, algorithm, and key tag for some DNSKEY RR in the zone's apex DNSKEY RRset.
+                    if (!dnsKeyRecord.Name.Equals(_signersName, StringComparison.OrdinalIgnoreCase))
+                        continue;
+
+                    DnsDNSKEYRecord dnsKey = dnsKeyRecord.RDATA as DnsDNSKEYRecord;
+
+                    if (dnsKey.Protocol != 3)
+                        continue;
+
+                    if ((dnsKey.Algorithm != _algorithm) || !dnsKey.PublicKey.IsAlgorithmSupported)
+                        continue;
+
+                    if (dnsKey.ComputedKeyTag != _keyTag)
+                        continue;
+
+                    //The matching DNSKEY RR MUST be present in the zone's apex DNSKEY RRset, and MUST have the Zone Flag bit (DNSKEY RDATA Flag bit 7) set.
+                    if (dnsKey.Flags.HasFlag(DnsDnsKeyFlag.ZoneKey))
                     {
-                        DnsDNSKEYRecord dnsKey = record.RDATA as DnsDNSKEYRecord;
+                        if (dnsKey.Flags.HasFlag(DnsDnsKeyFlag.Revoke) && (_typeCovered != DnsResourceRecordType.DNSKEY))
+                            continue; //rfc5011: the resolver MUST consider this key permanently invalid for all purposes except for validating the revocation.
 
-                        if (dnsKey.Flags.HasFlag(DnsDnsKeyFlag.ZoneKey) && (dnsKey.Protocol == 3) && (dnsKey.ComputedKeyTag == _keyTag))
+                        mS.Position = 0; //reset position before use
+
+                        if (dnsKey.PublicKey.IsSignatureValid(mS, _signature, hashAlgorithm))
                         {
-                            mS.Position = 0; //reset position before use
+                            foreach (DnsResourceRecord validatedRecord in records)
+                            {
+                                if (validatedRecord.Type == _typeCovered)
+                                    validatedRecord.SetDnssecStatus(DnssecStatus.Secure);
+                            }
 
-                            if (dnsKey.PublicKey.IsSignatureValid(mS, _signature, hashAlgorithm))
-                                return true;
+                            return DnssecSignatureStatus.Valid;
                         }
+
+                        isBogus = true;
                     }
                 }
 
-                return false;
+                if (isBogus)
+                {
+                    foreach (DnsResourceRecord validatedRecord in records)
+                    {
+                        if (validatedRecord.Type == _typeCovered)
+                            validatedRecord.SetDnssecStatus(DnssecStatus.Bogus);
+                    }
+
+                    return DnssecSignatureStatus.Bogus;
+                }
+
+                return DnssecSignatureStatus.NoDnsKey;
             }
         }
 
@@ -237,7 +407,7 @@ namespace TechnitiumLibrary.Net.Dns.ResourceRecords
                 if (_keyTag != other._keyTag)
                     return false;
 
-                if (!_signersName.Equals(other._signersName))
+                if (!_signersName.Equals(other._signersName, StringComparison.OrdinalIgnoreCase))
                     return false;
 
                 if (!BinaryNumber.Equals(_signature, other._signature))
@@ -268,7 +438,7 @@ namespace TechnitiumLibrary.Net.Dns.ResourceRecords
 
         public override string ToString()
         {
-            return (ushort)_typeCovered + " " + (byte)_algorithm + " " + _labels + " " + _originalTtl + " " + _signatureExpiration + " ( " + _signatureInception + " " + _keyTag + " " + _signersName + " " + Convert.ToBase64String(_signature) + " )";
+            return (ushort)_typeCovered + " " + (byte)_algorithm + " " + _labels + " " + _originalTtl + " " + _signatureExpiration + " " + _signatureInception + " " + _keyTag + " " + _signersName + ". " + Convert.ToBase64String(_signature) + " )";
         }
 
         #endregion
@@ -315,5 +485,41 @@ namespace TechnitiumLibrary.Net.Dns.ResourceRecords
         { get { return Convert.ToUInt16(2 + 1 + 1 + 4 + 4 + 4 + 2 + DnsDatagram.GetSerializeDomainNameLength(_signersName) + _signature.Length); } }
 
         #endregion
+
+        class SerializedResourceRecord : IComparable<SerializedResourceRecord>
+        {
+            #region variables
+
+            readonly byte[] _firstPart;
+            readonly byte[] _rdataPart;
+
+            #endregion
+
+            #region constructor
+
+            public SerializedResourceRecord(byte[] firstPart, byte[] rdataPart)
+            {
+                _firstPart = firstPart;
+                _rdataPart = rdataPart;
+            }
+
+            #endregion
+
+            #region public
+
+            public int CompareTo(SerializedResourceRecord other)
+            {
+                //Canonical RR Ordering by sorting RDATA portion of the canonical form of each RR
+                return DnsNSECRecord.CanonicalComparison(_rdataPart, other._rdataPart);
+            }
+
+            public void WriteTo(Stream s)
+            {
+                s.Write(_firstPart);
+                s.Write(_rdataPart);
+            }
+
+            #endregion
+        }
     }
 }
