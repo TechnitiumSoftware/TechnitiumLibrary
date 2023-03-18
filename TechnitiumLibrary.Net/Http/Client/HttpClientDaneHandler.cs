4bae265efbf8ba4430a3426376ff0b28ff3e5e67
Shreyas Zare <shreyas@technitium.com>
2023-03-18 11:18:58 +0530

    HttpClientDaneHandler: refactored implementation.

--- TechnitiumLibrary.Net/Http/Client/HttpClientDaneHandler.cs
+++ TechnitiumLibrary.Net/Http/Client/HttpClientDaneHandler.cs
@@ -20,46 +20,31 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 using System.Net.Http;
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
-using System.Threading;
-using System.Threading.Tasks;
 using TechnitiumLibrary.Net.Dns;
 
 namespace TechnitiumLibrary.Net.Http.Client
 {
-    public class HttpClientDaneHandler : HttpMessageHandler
+    public class HttpClientDaneHandler : DelegatingHandler
     {
         #region variables
 
         readonly DnsClient _dnsClient;
-        readonly HttpMessageInvoker _messageInvoker;
 
         #endregion
 
         #region constructor
 
-        public HttpClientDaneHandler()
+        public HttpClientDaneHandler(SocketsHttpHandler innerHandler, DnsClient dnsClient = null)
+            : base(innerHandler)
         {
-            _dnsClient = new DnsClient();
+            _dnsClient = dnsClient ?? new DnsClient();
             _dnsClient.DnssecValidation = true;
 
-            SocketsHttpHandler httpHandler = new SocketsHttpHandler();
-
-            httpHandler.SslOptions.RemoteCertificateValidationCallback += delegate (object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+            innerHandler.SslOptions.RemoteCertificateValidationCallback += delegate (object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
             {
                 _dnsClient.ValidateDaneAsync(sender as SslStream, certificate, chain, sslPolicyErrors).Sync();
                 return true;
             };
-
-            _messageInvoker = new HttpMessageInvoker(httpHandler);
-        }
-
-        #endregion
-
-        #region protected
-
-        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-        {
-            return _messageInvoker.SendAsync(request, cancellationToken);
         }
 
         #endregion
