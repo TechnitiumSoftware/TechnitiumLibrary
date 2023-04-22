b939290d77bb26e8cb6ca4f9d41c6c89438d18f2
Shreyas Zare <shreyas@technitium.com>
2023-04-22 18:18:42 +0530

    UdpClientConnection: implemented socket pooling to randomize query source port.

--- TechnitiumLibrary.Net/Dns/ClientConnection/UdpClientConnection.cs
+++ TechnitiumLibrary.Net/Dns/ClientConnection/UdpClientConnection.cs
@@ -20,8 +20,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 using System;
 using System.Diagnostics;
 using System.IO;
+using System.Net;
 using System.Net.Sockets;
 using System.Runtime.ExceptionServices;
+using System.Security.Cryptography;
 using System.Threading;
 using System.Threading.Tasks;
 using TechnitiumLibrary.Net.Proxy;
@@ -30,11 +32,97 @@ namespace TechnitiumLibrary.Net.Dns.ClientConnection
 {
     public class UdpClientConnection : DnsClientConnection
     {
+        #region variables
+
+        const int SOCKET_POOL_SIZE = 2500;
+        static PooledSocket[] _ipv4PooledSockets;
+        static PooledSocket[] _ipv6PooledSockets;
+        static readonly object _poolLock = new object();
+
+        #endregion
+
         #region constructor
 
         public UdpClientConnection(NameServerAddress server, NetProxy proxy)
             : base(DnsTransportProtocol.Udp, server, proxy)
         { }
+
+        #endregion
+
+        #region socket pool
+
+        public static void CreateSocketPool(bool enableIPv6)
+        {
+            lock (_poolLock)
+            {
+                if (_ipv4PooledSockets is null)
+                {
+                    PooledSocket[] pooledSockets = new PooledSocket[SOCKET_POOL_SIZE];
+
+                    for (int i = 0; i < SOCKET_POOL_SIZE; i++)
+                        pooledSockets[i] = new PooledSocket(AddressFamily.InterNetwork, i);
+
+                    _ipv4PooledSockets = pooledSockets;
+                }
+
+                if (enableIPv6)
+                {
+                    if (_ipv6PooledSockets is null)
+                    {
+                        PooledSocket[] pooledSockets = new PooledSocket[SOCKET_POOL_SIZE];
+
+                        for (int i = 0; i < SOCKET_POOL_SIZE; i++)
+                            pooledSockets[i] = new PooledSocket(AddressFamily.InterNetworkV6, i);
+
+                        _ipv6PooledSockets = pooledSockets;
+                    }
+                }
+                else
+                {
+                    if (_ipv6PooledSockets is not null)
+                    {
+                        foreach (PooledSocket pooledSocket in _ipv6PooledSockets)
+                            pooledSocket.DisposePooled();
+
+                        _ipv6PooledSockets = null;
+                    }
+                }
+            }
+        }
+
+        private static PooledSocket GetPooledSocket(AddressFamily addressFamily)
+        {
+            PooledSocket[] pooledSockets;
+
+            switch (addressFamily)
+            {
+                case AddressFamily.InterNetwork:
+                    pooledSockets = _ipv4PooledSockets;
+                    break;
+
+                case AddressFamily.InterNetworkV6:
+                    pooledSockets = _ipv6PooledSockets;
+                    break;
+
+                default:
+                    throw new NotSupportedException();
+            }
+
+            if (pooledSockets is null)
+                return new PooledSocket(addressFamily); //pooling not enabled; return new socket
+
+            int j = RandomNumberGenerator.GetInt32(SOCKET_POOL_SIZE);
+
+            for (int i = 0; i < SOCKET_POOL_SIZE; i++)
+            {
+                PooledSocket pooledSocket = pooledSockets[(j + i) % SOCKET_POOL_SIZE];
+                if (pooledSocket.TryUse())
+                    return pooledSocket; //return pooled socket
+            }
+
+            //no free pooled socket available; return new socket
+            return new PooledSocket(addressFamily);
+        }
 
         #endregion
 
@@ -99,11 +187,11 @@ namespace TechnitiumLibrary.Net.Dns.ClientConnection
                 if (_server.IsIPEndPointStale)
                     await _server.RecursiveResolveIPAddressAsync(null, null, false, DnsDatagram.EDNS_DEFAULT_UDP_PAYLOAD_SIZE, false, 2, 2000, cancellationToken);
 
-                using (Socket socket = new Socket(_server.IPEndPoint.AddressFamily, SocketType.Dgram, ProtocolType.Udp))
+                using (PooledSocket pooledSocket = GetPooledSocket(_server.IPEndPoint.AddressFamily))
                 {
                     stopwatch.Start();
 
-                    _ = await socket.UdpQueryAsync(new ArraySegment<byte>(sendBuffer, 0, sendBufferSize), receiveBuffer, _server.IPEndPoint, timeout, retries, false, IsResponseValid, cancellationToken);
+                    _ = await pooledSocket.Socket.UdpQueryAsync(new ArraySegment<byte>(sendBuffer, 0, sendBufferSize), receiveBuffer, _server.IPEndPoint, timeout, retries, false, IsResponseValid, cancellationToken);
 
                     stopwatch.Stop();
                 }
@@ -127,5 +215,90 @@ namespace TechnitiumLibrary.Net.Dns.ClientConnection
         }
 
         #endregion
+
+        class PooledSocket : IDisposable
+        {
+            #region variables
+
+            readonly Socket _socket;
+            readonly int _index;
+
+            int _inUse;
+
+            #endregion
+
+            #region constructor
+
+            public PooledSocket(AddressFamily addressFamily, int index = -1)
+            {
+                _socket = new Socket(addressFamily, SocketType.Dgram, ProtocolType.Udp);
+                _index = index;
+
+                if (index > -1)
+                {
+                    switch (addressFamily)
+                    {
+                        case AddressFamily.InterNetwork:
+
+                            try
+                            {
+                                _socket.Bind(new IPEndPoint(IPAddress.Any, RandomNumberGenerator.GetInt32(1000, ushort.MaxValue)));
+                            }
+                            catch (SocketException)
+                            {
+                                _socket.Bind(new IPEndPoint(IPAddress.Any, 0));
+                            }
+                            break;
+
+                        case AddressFamily.InterNetworkV6:
+                            try
+                            {
+                                _socket.Bind(new IPEndPoint(IPAddress.Any, RandomNumberGenerator.GetInt32(1000, ushort.MaxValue)));
+                            }
+                            catch (SocketException)
+                            {
+                                _socket.Bind(new IPEndPoint(IPAddress.IPv6Any, 0));
+                            }
+                            break;
+                    }
+                }
+            }
+
+            #endregion
+
+            #region IDisposable
+
+            public void Dispose()
+            {
+                if (_index < 0)
+                    _socket.Dispose(); //dispose non-pooled socket
+                else
+                    _inUse = 0; //free pooled socket
+            }
+
+            #endregion
+
+            #region public
+
+            public bool TryUse()
+            {
+                return Interlocked.CompareExchange(ref _inUse, 1, 0) == 0;
+            }
+
+            public void DisposePooled()
+            {
+                _inUse = 1; //to make TryUse() return false and thus prevent use
+                _socket.Dispose();
+            }
+
+            #endregion
+
+            #region properties
+
+            public Socket Socket
+            { get { return _socket; } }
+
+            #endregion
+        }
     }
 }
