f5a024617b8b2cf7f3ea43f1dc70d629f4eeb4ff
Shreyas Zare <shreyas@technitium.com>
2024-08-11 17:26:57 +0530

    Merged all array and list extension methods.

--- TechnitiumLibrary/CollectionExtensions.cs
+++ TechnitiumLibrary/CollectionExtensions.cs
@@ -19,35 +19,23 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Security.Cryptography;
 
 namespace TechnitiumLibrary
 {
-    public static class ListExtensions
+    public static class CollectionExtensions
     {
         public static void Shuffle<T>(this IList<T> array)
         {
-            Span<byte> buffer = stackalloc byte[4];
-
             int n = array.Count;
             while (n > 1)
             {
-                RandomNumberGenerator.Fill(buffer);
-                int k = (BitConverter.ToInt32(buffer) & 0x7FFFFFFF) % n--;
+                int k = RandomNumberGenerator.GetInt32(int.MaxValue) % n--;
                 T temp = array[n];
                 array[n] = array[k];
                 array[k] = temp;
             }
-        }
-
-        public static IList<T2> Convert<T1, T2>(this IList<T1> array, Func<T1, T2> convert)
-        {
-            T2[] newArray = new T2[array.Count];
-
-            for (int i = 0; i < array.Count; i++)
-                newArray[i] = convert(array[i]);
-
-            return newArray;
         }
 
         public static IReadOnlyList<T2> Convert<T1, T2>(this IReadOnlyList<T1> array, Func<T1, T2> convert)
@@ -60,7 +48,18 @@ namespace TechnitiumLibrary
             return newArray;
         }
 
-        public static bool Equals<T>(this IReadOnlyList<T> value1, IReadOnlyList<T> value2)
+        public static IReadOnlyCollection<T2> Convert<T1, T2>(this IReadOnlyCollection<T1> collection, Func<T1, T2> convert)
+        {
+            T2[] newArray = new T2[collection.Count];
+            int i = 0;
+
+            foreach (T1 item in collection)
+                newArray[i++] = convert(item);
+
+            return newArray;
+        }
+
+        public static bool ListEquals<T>(this IReadOnlyList<T> value1, IReadOnlyList<T> value2)
         {
             if (ReferenceEquals(value1, value2))
                 return true;
@@ -78,6 +77,39 @@ namespace TechnitiumLibrary
             }
 
             return true;
+        }
+
+        public static bool HasSameItems<T>(this IReadOnlyCollection<T> value1, IReadOnlyCollection<T> value2)
+        {
+            if (ReferenceEquals(value1, value2))
+                return true;
+
+            if ((value1 is null) || (value2 is null))
+                return false;
+
+            if (value1.Count != value2.Count)
+                return false;
+
+            foreach (T item in value1)
+            {
+                if (!value2.Contains(item))
+                    return false;
+            }
+
+            return true;
+        }
+
+        public static int GetArrayHashCode<T>(this IReadOnlyCollection<T> value)
+        {
+            if (value is null)
+                return 0;
+
+            int hashCode = 0x7B58D9E4;
+
+            foreach (T t in value)
+                hashCode ^= t.GetHashCode();
+
+            return hashCode;
         }
     }
 }
