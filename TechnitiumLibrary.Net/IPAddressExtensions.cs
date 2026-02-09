0055ec752b45c42f8ec7e0ebbb4dd9fde2d034d6
Zafer Balkan <zafer@zaferbalkan.com>
2026-02-09 11:31:39 +0200

    Revert "Added copyright"
    This reverts commit a59e4a6584d3c1db53dc6ba76808ab5354d56057.

--- TechnitiumLibrary.Net/IPAddressExtensions.cs
+++ TechnitiumLibrary.Net/IPAddressExtensions.cs
@@ -1,7 +1,6 @@
 ﻿/*
 Technitium Library
 Copyright (C) 2025  Shreyas Zare (shreyas@technitium.com)
-Copyright (C) 2026  Zafer Balkan (zafer@zaferbalkan.com)
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
