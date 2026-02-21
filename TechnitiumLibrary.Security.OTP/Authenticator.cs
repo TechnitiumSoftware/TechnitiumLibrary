e2a4a232696cdceb8d88d8372579841e84a1cf22
Shreyas Zare <shreyas@technitium.com>
2026-02-21 17:02:03 +0530

    minor change

--- TechnitiumLibrary.Security.OTP/Authenticator.cs
+++ TechnitiumLibrary.Security.OTP/Authenticator.cs
@@ -1,6 +1,6 @@
 ﻿/*
 Technitium Library
-Copyright (C) 2025  Shreyas Zare (shreyas@technitium.com)
+Copyright (C) 2026  Shreyas Zare (shreyas@technitium.com)
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
